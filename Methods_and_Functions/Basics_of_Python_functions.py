f3a94b3c5418606c3a86936bc04c351787c790f5
David Santiago <56009621+davidsantiago01@users.noreply.github.com>
2025-10-03 23:57:14 -0600

    Create Basics_of_Python_functions.py

---
+++ Methods_and_Functions/Basics_of_Python_functions.py
@@ -0,0 +1,17 @@
+def say_hello():
+    print ("hello")
+    print ("how are you?")
+    print ("are you having a good day?")
+
+say_hello()
+hello
+how are you?
+are you having a good day?
+
+#---
+def say_hello(name):
+    print (f"hello {name}")
+say_hello("Dave")
+hello Dave
+#----
+
