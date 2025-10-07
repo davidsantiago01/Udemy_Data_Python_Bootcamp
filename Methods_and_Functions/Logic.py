304d8591a27fc91d1a12d38dc2b9005947b13d4a
David Santiago <56009621+davidsantiago01@users.noreply.github.com>
2025-10-06 23:59:46 -0600

    Update Logic.py

--- Methods_and_Functions/Logic.py
+++ Methods_and_Functions/Logic.py
@@ -7,3 +7,12 @@ def even_check(num):
 even_check(20)  
 True
 #-----
+def check_even_list(num_list):
+    for num in num_list:
+        if num % 2 == 0:
+            return True
+    else:
+        pass
+check_even_list([2,5,8])
+True
+#-----
