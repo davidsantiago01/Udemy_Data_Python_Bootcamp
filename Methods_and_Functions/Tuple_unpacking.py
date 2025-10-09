310710fa166804b4a999ed12fc4f73b9e8532222
David Santiago <56009621+davidsantiago01@users.noreply.github.com>
2025-10-09 00:00:08 -0600

    Update Tuple_unpacking.py

--- Methods_and_Functions/Tuple_unpacking.py
+++ Methods_and_Functions/Tuple_unpacking.py
@@ -13,3 +13,19 @@ for ticker, price in stock_prices:
 110.0
 #-----
 
+work_hours = [('Abby', 100), ('Billy', 400), ('Cassie', 800)]
+def employee_check(work_hours):
+    current_max = 0
+    employee_of_month = ""
+    for employee, hours in work_hours:
+        if hours > current_max:
+            current_max = hours
+            employee_of_month = employee
+        else:
+            pass
+    return (employee_of_month, current_max)
+
+employee_check(work_hours)
+('Cassie', 800)
+#--------
+
