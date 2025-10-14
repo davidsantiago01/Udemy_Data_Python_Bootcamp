3b591a98a24f036c57a6f03e381fa5c748ce9ffc
David Santiago <56009621+davidsantiago01@users.noreply.github.com>
2025-10-13 23:58:01 -0600

    Update Interactions_between_Py_functions.py

--- Methods_and_Functions/Interactions_between_Py_functions.py
+++ Methods_and_Functions/Interactions_between_Py_functions.py
@@ -9,8 +9,44 @@ example
 def shuffle_list(mylist):
     shuffle(mylist)
     return mylist
-shuffle_list(example)
+result = shuffle_list(example)
+result
 [8, 1, 6, 9, 5, 10, 7, 3, 4, 2]
 
 #---
+
+my_list = [' ', 0, ' ']
+shuffle_list(my_list)
+[' ', ' ', 0]
+
+def player_guess():
+    guess = ''
+    while guess not in ['0','1','2']:
+        guess = input ("Pick a number: 0, 1 or 2: ")
+    return int(guess)
+player_guess()
+my_index = player_guess()
+my_index
+
+def check_guess(my_guess, my_list):
+    if my_list[my_guess] == 0:
+        print ("Correct!")
+    else:
+        print ("Wrong guess!")
+        print (my_list)
+my_list = [' ', 0, ' ']
+mixedup_list = shuffle_list(my_list)
+guess = player_guess()
+check_guess(guess, mixedup_list)
+#First attempt with #1
+Correct!
+#Second attempt with #0
+Wrong guess!
+[' ', ' ', 0]
+
+#---------------
+
+
+
+
 
